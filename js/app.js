7e1aff88fda894bec2296516d6661785d907a98e
DariaRusovich <72560988+DariaRusovich@users.noreply.github.com>
2021-05-17 19:29:03 +0300

    start-project

---
+++ js/app.js
@@ -0,0 +1,64 @@
+const CARS = JSON.parse(DATA)
+const carList = document.getElementById('carList')
+// CARS.length = 50
+console.log(CARS);
+
+
+
+
+
+renderCards(createCardsHTML(CARS), carList)
+
+
+
+
+
+function createCardsHTML(carsArray) {
+    let cardsHTML = ''
+    carsArray.forEach(car => {
+        cardsHTML += createCard(car)
+    })
+    return cardsHTML
+}
+
+function renderCards(cardsHtml, carListElement) {
+    carListElement.insertAdjacentHTML('beforeend', cardsHtml)
+}
+
+function createCard(carData) {
+    return `<div class="card mb-3">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img loading="lazy" width="1" height="1" class="card-img car-img" src="${carData.img}" alt="${carData.make} ${carData.model}">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${carData.make} ${carData.model} ${carData.engine_volume} (${carData.year})</h5>
+          <h6>${carData.price}$</h6>
+          <a href="tel:${carData.phone}" class="btn btn-success"><i class="bi bi-telephone-forward-fill me-2"></i>Call seller</a>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="card-footer text-muted d-flex justify-content-between align-items-center">
+          <div>
+            <small class="me-3"><i class="bi bi-clock-fill me-1"></i>${carData.timestamp}</small>
+            <small><i class="bi bi-person-check-fill me-1"></i>${carData.seller}</small>
+          </div>
+          <small><i class="bi bi-eye-fill me-1"></i>${carData.views}</small>
+        </div>
+      </div>
+    </div>
+    </div>`
+}
+
+
+
+
+// const arr = [0,1,2,3,4,5,6,7,5,8,9]
+
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     console.log(element);
+// }
+
+// arr.forEach(element => console.log(element))
